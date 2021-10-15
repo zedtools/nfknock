4ef48c09c24ffe37192f537cb595d34fa007dcde
Zoltan Celedes <zoltanc@gmail.com>
2021-10-15 17:22:23 +1100

    Fixed order of command line arguments to s-nail

--- logmail.sh
+++ logmail.sh
@@ -24,7 +24,7 @@ then
 	# look up whois data for the remote client
 	WHOIS_DATA=$(whois $IP)
 
-	echo -e "$MSG [SRC = $IP]\n\nLog entry: $LOG\n\nWHOIS:\n\n$WHOIS_DATA" | s-nail -S v15-compat -S smtp-auth=none -S from="$MAILFROM" -S mta=$SMTP_SERVER "$MAILTO" -s "$(hostname): Knock from $IP"
+	echo -e "$MSG [SRC = $IP]\n\nLog entry: $LOG\n\nWHOIS:\n\n$WHOIS_DATA" | s-nail -S v15-compat -S smtp-auth=none -S from="$MAILFROM" -S mta=$SMTP_SERVER -s "$(hostname): Knock from $IP" "$MAILTO"
 else
 	echo "Error: environment variables SMTP_SERVER, MAILFROM and MAILTO must be set" >> $ERR_LOG
 fi
